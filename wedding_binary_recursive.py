f987458c8696c0f37b601bc8ba5194a2a61d8268
ninjalante <77864271+ninjalante@users.noreply.github.com>
2023-03-31 18:34:22 +0900

    add movies, change questions

--- wedding_binary_recursive.py
+++ wedding_binary_recursive.py
@@ -13,7 +13,7 @@ ed_helms = wm.Question("\nThe kids aren't there. So. How raunchy do you want it?
 crash = wm.Question("\nHmm. I've only got two movies left. I guess...the real question is...do you want to watch something you've probably already seen, or something that you slept on?\nA) give me what I came here expecting\nB) give me something new\n", 'A Buddy Comedy That Doesn\'t Have Ed Helms')
 sandler = wm.Question("\nDo you like Adam Sandler?\nA) Love Adam Sandler\nB) Who?\n", 'Not a buddy comedy')
 nineties = wm.Question("\nWow, really? Huh. Okay. Then, uh...\nA) 1990s Adam Sandler\nB) 2010s Adam Sandler?\n", 'I Stan-dler Adam Sandler')
-mature_sandler = wm.Question("\nAmazingly, you've got two options. Do you want to see Adam Sandler...\nA) At like a mature and concerned father\nB) Act like an immature and unconcerned father\n", "2010s Adam Sandler")
+mature_sandler = wm.Question("\nAmazingly, you've got two options. Do you want to see Adam Sandler...\nA) Act like a mature and concerned father\nB) Act like an immature and unconcerned father\n", "2010s Adam Sandler")
 timeline = wm.Question("\nAre you a fan of time loops and alternate timelines?\nA) yes\nB) no\n", 'Not  A-damn Sandler')
 time = wm.Question("\nTime stuff, excellent.\nA) a time loop a la Groundhog Day?\nB) alternate timelines a la Sliding Doors?\n", 'Timeline wackiness')
 stiller = wm.Question("\nWho do you prefer as a leading man?\nA) Ben Stiller\nB) Ryan Reynolds\n", 'Normal time stuff please')
@@ -21,15 +21,25 @@ female_protag = wm.Question("\nFemale leads it is.\nDo you want...\nA) a family-
 f_f_f = wm.Question("\nCool. You have some good options.\nAre you looking for...\nA) something nostalgic\nB) something fresh and new!\n", 'Family-friendly')
 nostalgic = wm.Question("\nDo you want a lot of singing and dancing?\nA) Yes! A musical!\nB) Not my thing\n", 'Something nostalgic')
 body_swap = wm.Question("\nDo you enjoy the whimsical magic of a body swap?\nA) Body swap magic please\nB) No thanks, I just want a good wedding\n", 'No singing')
-f_not_f_f = wm.Question("\nGreat, that will make it easier. You've got some very different options, so let's narrow it down. Do you want...\nA) a story with interesting characters and conflicts\nB) a story with unnecessary amounts of GLAMOUR and DRAMA\n", 'Something just for me and the girls')
-substance = wm.Question("\nI don't mean to assume, but most wedding movies aren't exactly...thought-provoking. Do you want a movie that...\nA) is easy-breezy and full of shenanigans\nB) introduces complicated characters and difficult situations\n", "Character and conflict")
-wedding = wm.Question("\nPerfect! One question left. Choose your leading man.\nA) Matthew McConaghey\nB) James Marsden\nC) Neither\n", "Easy breezy shenanigans")
-drama = wm.Question("\nDo you want to indulge in some really obscene opulance?\nA) Yes, smother me with capitalist fantasy\nB) No, I want to be reminded how poor I am\n", "Here for the drama")
+f_not_f_f = wm.Question("\nGreat, that will make it easier. You've got some very different options, so let's narrow it down. Do you want...\nA) complicated characters and difficult situations\nB) easy-to-consume motivations I won't have to think about much\n", 'Not family-friendly')
+substance = wm.Question("\nBut, like, how complicated and how difficult?\nA) Really dramatic. I want it to hurt\nB) Eh. It can have a side of fun\n", "Something with substance")
+drama = wm.Question("\nWhich of these storylines appeals to you more?\nA) A recovering addict seeing her family for the first time in years\n B) Former high school classmates seeing each other for the first time in years\n", "Drama-rama")
+more_fun = wm.Question("\nHow much do you want to be disgusted by capitalist propaganda and consumer culture?\nA) I want an obscene capitalist fantasy\nB) I want to be reminded that every job is a pyramid scheme\n", "More fun")
+easy_to_consume = wm.Question("\nI don't blame you. Do you want...\nA) Something you probably watched on a plane 15 years ago\nB) Something that's probably new to you\n", "Easy to consume")
+plane15 = wm.Question("\nLast thing! Pick your preferred leading man.\n A) Matthew McConaghey\nB) James Marsden\n", "Watched on a plane 15 years ago")
+new2you = wm.Question("One more question. Do you want a movie that's...\nA) Laugh-out-loud outrageous\nB) Sweet and comforting\n", "New to me")
 obscene = wm.Question("\nLast question. Are you...\nA) a Carrie\nB) a Samantha\nC) a Miranda\nD) a Charlotte\n", "Obscene capitalist fantasy please")
-nostalgic_or_modern = wm.Question("\nI've got some ideas. Do you want...\nA) a nostalgic classic\nB) something more modern\n", "Two protagonists")
+#####
+stop_motion = wm.Question("\nFirst question! Do you want...\nA) a whimsical stop-motion piece that's fun for the whole family\nB) literally anything else\n", "One couple")
+nostalgic_or_modern = wm.Question("\nI've got some ideas. Do you want...\nA) a nostalgic classic\nB) something more modern\n", "Live-action")
 cynical = wm.Question("\nAlmost done! Are you feeling...\nA) a somewhat cynical, but ultimately sweet, and certainly more realistic, look at new relationships\nB) 100% bonkers fairy tale\n", "Something new")
-cringe = wm.Question("\nI think I've got something for you, but tell me, do you like extremely relevant cringe comedy?\nA) no thank you\nB) BRING ON THE CRINGE!\n", "More realistic")
+
+fairy_tale = wm.Question("\nOne more question! What kind of fantasy do you need today?\nA) A non-stop and very ridiculous thrill ride\nB) A romance that will endear itself to you more than you expected\n", "100% bonkers fairy tale")
+cringe = wm.Question("\nI think I've got something for you, but tell me, do you like extremely relevant cringe comedy?\nA) nothankyou\nB) BRING ON THE CRINGE!\n", "More realistic")
+
 how_realistic = wm.Question("\nLow cringe, ok. But like, HOW realistic?\nA) Mostly real\nB) I wouldn't mind some timeline wackiness\n", "No cringe please")
+baggage = wm.Question("\nWhat kind of couple appeals to you most?\nA) Exes with baggage\nB) Strangers with baggage\nC) Siblings with baggage\n", "Mostly realistic")
+
 hidden_gem = wm.Question("\nI think I've got it. Tell me one more thing...\nA) I want an instantly recognizable classic\nB) I want a hidden gem that I can impress my snootier friends with\n", "Ensemble cast")
 italian_or_indian = wm.Question("\nThis last question should be easy. You're about to watch a wedding movie with your friends or family. What take-out do you get?\nA) Italian\nB) Indian\n", "Something new")
 
@@ -38,24 +48,41 @@ italian_or_indian = wm.Question("\nThis last question should be easy. You're abo
 root_q.a = male_protag
 root_q.b = female_protag
 root_q.c = ensemble
-ensemble.a = nostalgic_or_modern
+
+ensemble.a = stop_motion
+stop_motion.a = wm.corpse_bride
+stop_motion.a.name = "A whimsical stop-motion film"
+stop_motion.b = nostalgic_or_modern
+
 ensemble.b = hidden_gem
 hidden_gem.a = wm.four_ws_1f
 hidden_gem.a.name = "Something classic"
 hidden_gem.b = wm.monsoon
 hidden_gem.b.name = "Something lesser-known"
+
 nostalgic_or_modern.a = wm.princess
+wm.princess.name = "A nostalgic classic"
 nostalgic_or_modern.b = cynical
 cynical.a = cringe
 cringe.a = how_realistic
-how_realistic.a = wm.destination
-how_realistic.a.name = "Mostly realistic"
+
+how_realistic.a = baggage
 how_realistic.b = wm.palm_springs
 how_realistic.b.name = "I'm okay with some wackiness"
+baggage.a = wm.ticket2paradise
+baggage.a.name = "Exes with baggage"
+baggage.b = wm.destination
+baggage.b.name = "Strangers with baggage"
+baggage.c = wm.people_we_hate
+baggage.c.name = "Siblings with baggage"
+
 cringe.b = wm.you_people
 cringe.b.name = "I want to curl into a ball from cringing so hard"
-cynical.b = wm.marry_me
-cynical.b.name = "A love story so unbelievable that I can't help but enjoy it"
+cynical.b = fairy_tale
+fairy_tale.a = wm.shotgun
+fairy_tale.a.name = "Something truly ridiculous"
+fairy_tale.b = wm.marry_me
+fairy_tale.b.name = "A love story so unbelievable that I can't help but enjoy it"
 female_protag.a = f_f_f
 f_f_f.a = nostalgic
 f_f_f.b = italian_or_indian
@@ -65,13 +92,23 @@ italian_or_indian.b = wm.wedding_season
 italian_or_indian.b.name = 'Indian food'
 female_protag.b = f_not_f_f
 f_not_f_f.a = substance
-f_not_f_f.b = drama
-substance.a = wedding
-substance.b = wm.rachel
-substance.b.name = 'Something with substance'
-drama.a = obscene
-drama.b = wm.bridesmaids
-drama.b.name = "Help me, I'm poor"
+f_not_f_f.b = easy_to_consume
+easy_to_consume.a = plane15
+easy_to_consume.b = new2you
+new2you.a = wm.rough_night
+new2you.a.name = "Laugh-out-loud outrageous"
+new2you.b = wm.muriels
+new2you.b.name = "Sweet and comforting"
+###
+substance.a = drama
+drama.a = wm.rachel
+drama.a.name = "Recovering alcoholic"
+drama.b = wm.bachelorette
+drama.b.name = "Recovering high schoolers"
+substance.b = more_fun
+more_fun.a = obscene
+more_fun.b = wm.bridesmaids
+more_fun.b.name = "Help me I'm poor"
 obscene.a = wm.satc
 obscene.a.name = "such a Carrie"
 obscene.b = wm.satc
@@ -80,12 +117,11 @@ obscene.c = wm.satc
 obscene.c.name = "totally Miranda"
 obscene.d = wm.crazy_rich_a
 obscene.d.name = "a real Charlotte"
-wedding.a = wm.wedding_planner
-wedding.a.name = 'McConaghey'
-wedding.b = wm.dresses27
-wedding.b.name = "Marsden"
-wedding.c = wm.muriels
-wedding.c.name = "I don't care about a leading man, I'm here for Toni Collette"
+plane15.a = wm.wedding_planner
+plane15.a.name = 'McConaghey'
+plane15.b = wm.dresses27
+plane15.b.name = "Marsden"
+##########
 nostalgic.a = wm.mamma_mia
 nostalgic.a.name = 'A musical extravaganza'
 nostalgic.b = body_swap
@@ -93,6 +129,7 @@ body_swap.a = wm.freaky_friday
 body_swap.a.name = 'Body swap hijinks'
 body_swap.b = wm.bfgw
 body_swap.b.name = 'I want a wedding that\'s a good time'
+#########
 male_protag.a = m_f_f
 male_protag.b = m_not_f_f
 m_f_f.a = classic
@@ -176,7 +213,6 @@ def find_a_movie(movie_tree, path=[]):
     path += [movie_tree.d.name]
     movie_tree = movie_tree.d
     return find_a_movie(movie_tree, path)
-  
 
 start = input('Are you ready for wedding season?\n')
 while start.upper() == 'YES': 
